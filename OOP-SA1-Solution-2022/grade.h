ddad2972087ee18317cc81236ab607fda90e217e
Ryan Tempest <246464@student.lincolncollege.ac.uk>
2023-01-05 01:30:41 +0000

    Test are working for the 3rd, will now start coding

--- OOP-SA1-Solution-2022/grade.h
+++ OOP-SA1-Solution-2022/grade.h
@@ -30,6 +30,15 @@ public:
 
 
 	// 3rd Methods
+	
+	// sum methods 
+	long double sum(const std::vector<double>&);
+	long double sum(const std::vector<int>&);
+
+	// mean + population variance methods
+	double mean(const std::vector<double>&);
+	double mean(const std::vector<int>&);
+	double populationVariance(const std::vector<double>&, double mean);
 
 	// 2.2 Methods
 
@@ -43,6 +52,22 @@ public:
 };
 
 // Class Implementation
+
+long double Grade::sum(const std::vector<double>& values) {
+	return 0;
+}
+long double Grade::sum(const std::vector<int>& values) {
+	return 0;
+}
+double Grade::mean(const std::vector<double>& values) {
+	return 0;
+}
+double Grade::mean(const std::vector<int>& values) {
+	return 0;
+}
+double Grade::populationVariance(const std::vector<double>& values, double mean) {
+	return 0;
+}
 
 // Sample code passed test
 double Grade::add(double dataItem1, double dataItem2) {
